1d7fc26e823007b08053b4accff2c624229be45f
tharunthomas8 <tharunthomas8@gmail.com>
2018-07-21 06:14:26 +0530

    Upload files

--- add_book/urls.py
+++ add_book/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^enter_details/$', views.add_book, name="add-book"),
     url(r'^list_books/$', views.list_book, name="list_books"),
 ]
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
